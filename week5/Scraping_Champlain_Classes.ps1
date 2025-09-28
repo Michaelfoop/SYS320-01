0586b015dde038abbcb28b18ccd3fd6729bca7ad
Michael <michaelrtmehl@gmail.com>
2025-09-28 19:25:14 -0400

    Assignment 2

--- week5/Scraping_Champlain_Classes.ps1
+++ week5/Scraping_Champlain_Classes.ps1
@@ -25,3 +25,55 @@ for($i=1; $i -lt $trs.length; $i++){
 
 return $FullTable
 }
+
+
+
+# Days Translator functio 
+function daysTranslator($FullTable){
+
+for($i=0;$i -lt $FullTable.length; $i++){
+    $Days=@()
+
+    if($FullTable[$i].Days -ilike "M*"){ $Days += "Monday"}
+
+    if($FullTable[$i].Days -ilike "*T[W,F,T]*"){ $Days += "Tuesday"}
+    ElseIf($FullTable[$i].Days -ilike "T"){ $Days += "Tuesday"}
+
+    if($FullTable[$i].Days -ilike "*W*"){ $Days += "Wednesday"}
+
+    if($FullTable[$i].Days -ilike "*TH*"){ $Days += "Thursday"}
+
+    if($FullTable[$i].Days -ilike "*F"){ $Days += "Friday"}
+
+    $FullTable[$i].Days = $Days
+    }
+return $FullTable
+}
+
+
+$one = gatherClasses
+$two = daysTranslator($one)
+
+# one
+#$two | Where-Object {$_.Instructor -ilike "*Furkan*"}
+
+
+# two
+#$two | Where-Object { ($_.Location -eq "JOYC 310") -and ($_.Days -contains "Monday") } |
+#       Sort-Object "Time Start" | Select-Object "Time Start", "Time End", "Class Code"
+
+# three
+$bug = $two | Where-Object { ($_."Class Code" -ilike "SYS*") -or
+                             ($_."Class Code" -ilike "NET*") -or
+                             ($_."Class Code" -ilike "SEC*") -or
+                             ($_."Class Code" -ilike "FOR*") -or
+                             ($_."Class Code" -ilike "CSI*") -or
+                             ($_."Class Code" -ilike "DAT*") } | Select-Object -Unique "Instructor" | Sort-Object Instructor  
+
+
+
+# Four
+$two | Where-Object { $_.Instructor -in $bug.Instructor } |
+       Group-Object "Instructor" | Select-Object Count, Name | Sort-Object Count -Descending
+
+
