df4a4b8f668db376973cb8ce5f72b77ee2fc4af1
Michael <michaelrtmehl@gmail.com>
2025-10-05 23:13:39 -0400

    week6 part 2

---
+++ week6/chromeThingy.ps1
@@ -0,0 +1,8 @@
+﻿function chromeThing{
+    if (Get-Process -Name chrome -ErrorAction SilentlyContinue){
+        Stop-Process -Name chrome -Force
+        }
+        else{
+            Start-Process chrome "https://www.champlain.edu"
+        }
+}
---
+++ week6/turn_to_menu.ps1
@@ -0,0 +1,59 @@
+﻿. (Join-Path $PSScriptRoot Event-Logs.ps1)
+. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot chromeThingy.ps1)
+
+
+$Prompt = "`n"
+$Prompt += "Choose Below`n"
+$Prompt += "1 - Display Last 10 Apache Logs`n"
+$Prompt += "2 - Display Last 10 Failed Logins for all Users`n"
+$Prompt += "3 - Display at Risk Users`n"
+$Prompt += "4 - Start Chrome Web Browser and Navigate to champlain.edu`n"
+$Prompt += "5 - Exit`n"
+
+
+$loop = $true
+
+while ($loop){
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host
+
+    if($choice -eq 5){
+        Write-Host "Bye Bye" | Out-String
+        exit
+        $operation = $false
+    }
+
+
+    elseif($choice -eq 1){
+        $loc="C:\xampp\apache\logs\*.log"
+        if(Test-Path $loc){
+            Get-Content $loc -Tail 10
+        }
+        else{
+            Write-Host "nothin"
+        }
+    }
+
+    elseif($choice -eq 2){               
+        $userLogins = getFailedLogins(100)
+        Write-Host ($userLogins | Group-Object -Property User | ForEach-Object { $_.Group | Sort-Object -Property Time -Descending | Select-Object -First 10 } | Format-Table User, Time, Event | Out-String)
+    }
+
+    elseif($choice -eq 3){
+        $day = Read-Host -Prompt "How many days back into the logs"
+        Write-Host "Number of Users with 10 or more failed log ins at $day days"
+        badGuys $day
+    }
+
+    elseif($choice -eq 4){
+        chromeThing
+    }
+
+    else{
+        Write-Host "Try again"
+        $operation = $true
+    }
+
+}
